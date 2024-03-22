e44790783681f4fa34feb687674a08c769151882
miguelangel17z <158492403+miguelangel17z@users.noreply.github.com>
2024-03-22 11:25:35 -0500

    Add files via upload

---
+++ Healtime.java
@@ -0,0 +1,18 @@
+// Healtime.java
+package codigo;
+
+import interfaz_grafica.Interfaz;
+
+
+
+public class Healtime {
+
+    public static void main(String[] args) {
+        
+        
+Interfaz panta = new Interfaz();
+      panta.setVisible(true);
+panta.setLocationRelativeTo(null);
+     
+    }
+}
