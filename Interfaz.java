dbade6e2320e81feba44c7191655b4903bf04bdc
miguelangel17z <158492403+miguelangel17z@users.noreply.github.com>
2024-04-27 20:54:34 -0500

    Update Interfaz.java

--- Interfaz.java
+++ Interfaz.java
@@ -19,6 +19,7 @@ public class Interfaz extends javax.swing.JFrame {
     
   conexion.ConexionMySql con = new ConexionMySql();
   Connection cn = con.conectar();
+  public static String UsernameGuardado = "";
 
     public static Registro cambio;
     public static Pantalla1 primera;
@@ -27,7 +28,7 @@ public class Interfaz extends javax.swing.JFrame {
     }
     
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
@@ -38,8 +39,8 @@ public class Interfaz extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         txtPassword = new javax.swing.JPasswordField();
         jLabel5 = new javax.swing.JLabel();
-        inicio1 = new javax.swing.JButton();
-        Registro = new javax.swing.JButton();
+        login_Iniciarpantalla1 = new javax.swing.JButton();
+        abrirRegistro = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -100,28 +101,28 @@ public class Interfaz extends javax.swing.JFrame {
         jLabel5.setText("jLabel5");
         jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 0, 597, 700));
 
-        inicio1.setBackground(new java.awt.Color(0, 0, 0));
-        inicio1.setFont(new java.awt.Font("Montserrat", 0, 24)); // NOI18N
-        inicio1.setForeground(new java.awt.Color(255, 255, 255));
-        inicio1.setText("INICIAR SESIÓN");
-        inicio1.addActionListener(new java.awt.event.ActionListener() {
+        login_Iniciarpantalla1.setBackground(new java.awt.Color(0, 0, 0));
+        login_Iniciarpantalla1.setFont(new java.awt.Font("Montserrat", 0, 24)); // NOI18N
+        login_Iniciarpantalla1.setForeground(new java.awt.Color(255, 255, 255));
+        login_Iniciarpantalla1.setText("INICIAR SESIÓN");
+        login_Iniciarpantalla1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                inicio1ActionPerformed(evt);
+                login_Iniciarpantalla1ActionPerformed(evt);
             }
         });
-        jPanel1.add(inicio1, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 540, 250, 52));
+        jPanel1.add(login_Iniciarpantalla1, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 540, 250, 52));
 
-        Registro.setBackground(new java.awt.Color(0, 0, 0));
-        Registro.setFont(new java.awt.Font("Montserrat", 0, 18)); // NOI18N
-        Registro.setForeground(new java.awt.Color(102, 102, 255));
-        Registro.setText("No tienes una cuenta?registrate aqui.");
-        Registro.setActionCommand("");
-        Registro.addActionListener(new java.awt.event.ActionListener() {
+        abrirRegistro.setBackground(new java.awt.Color(0, 0, 0));
+        abrirRegistro.setFont(new java.awt.Font("Montserrat", 0, 18)); // NOI18N
+        abrirRegistro.setForeground(new java.awt.Color(102, 102, 255));
+        abrirRegistro.setText("No tienes una cuenta?registrate aqui.");
+        abrirRegistro.setActionCommand("");
+        abrirRegistro.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                RegistroActionPerformed(evt);
+                abrirRegistroActionPerformed(evt);
             }
         });
-        jPanel1.add(Registro, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 640, 390, 40));
+        jPanel1.add(abrirRegistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 640, 390, 40));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -135,20 +136,20 @@ public class Interfaz extends javax.swing.JFrame {
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>                        
 
-    private void txtPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPasswordActionPerformed
+    private void txtPasswordActionPerformed(java.awt.event.ActionEvent evt) {                                            
         // TODO add yo handling code here:
-    }//GEN-LAST:event_txtPasswordActionPerformed
+    }                                           
 
-    private void RegistroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RegistroActionPerformed
+    private void abrirRegistroActionPerformed(java.awt.event.ActionEvent evt) {                                              
                cambio = new Registro();
         cambio.setVisible(true);
         cambio.setLocationRelativeTo(null);
+     
+    }                                             
 
-    }//GEN-LAST:event_RegistroActionPerformed
-
-    private void inicio1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inicio1ActionPerformed
+    private void login_Iniciarpantalla1ActionPerformed(java.awt.event.ActionEvent evt) {                                                       
                                       
         
     String usuario = txtUsuario.getText();
@@ -166,9 +167,11 @@ public class Interfaz extends javax.swing.JFrame {
    if (rs.next()) {
         String nombre = rs.getString("nombre");
           JOptionPane.showMessageDialog(null, "Inicio de sesión exitoso. Bienvenido, " + nombre);
+          UsernameGuardado = usuario;
           primera = new Pantalla1();
         primera.setVisible(true); 
         primera.setLocationRelativeTo(null);
+       
 
           
     } else {
@@ -186,19 +189,19 @@ public class Interfaz extends javax.swing.JFrame {
     
         
         
-    }//GEN-LAST:event_inicio1ActionPerformed
+    }                                                      
 
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Registro;
-    private javax.swing.JButton inicio1;
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton abrirRegistro;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JButton login_Iniciarpantalla1;
     private javax.swing.JPasswordField txtPassword;
     private javax.swing.JTextArea txtUsuario;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration                   
 }
